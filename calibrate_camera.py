f0177263fa6f55f6e0d8c760b495fa92ccced477
Patrick Poon <ppoon@demandware.com>
2017-04-06 20:33:53 -0400

    Re-wrote calibrate_camera

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -1,54 +1,38 @@
 import cv2
-import glob
+from glob import iglob
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-# %matplotlib qt
-# %matplotlib inline
+
+def get_corners(gray_image, pts_per_row, pts_per_column, flags=None):
+    return cv2.findChessboardCorners(gray_image, (pts_per_row, pts_per_column), flags)
 
 
-class ImageSample():
-
-    def __init__(self, filename):
-        self.filename = filename
-        self.image = self.read_image(self.filename)
-        self.size = (self.image.shape[1], self.image.shape[0])
-
-    def read_image(self, filename):
-        return cv2.imread(filename)
+def draw_image_corners(image, pts_per_row, pts_per_column, corners, ret):
+    cornered = cv2.drawChessboardCorners(image, (pts_per_row, pts_per_column), corners, ret)
+    return cornered
 
 
-class Calibrator():
-
-    def __init__(self, calibration):
-        self.ret = calibration[0]
-        self.mtx = calibration[1]
-        self.dist = calibration[2]
-        self.rvecs = calibration[3]
-        self.tvecs = calibration[4]
-
-
-def get_calibration_points(images_regex, num_channels, x, y, z=0):
+def get_calibration_pts(img_files_regex, pts_per_row, pts_per_column, num_channels):
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((y * x, num_channels), np.float32)
-    objp[:, :2] = np.mgrid[0:x, 0:y].T.reshape(-1, 2)
+    objp = np.zeros((pts_per_column * pts_per_row, 3), np.float32)
+    objp[:,:2] = np.mgrid[0:pts_per_row, 0:pts_per_column].T.reshape(-1, 2)
 
     # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d points in real world space
-    imgpoints = []  # 2d points in image plane.
+    objpoints = [] # 3d points in real world space
+    imgpoints = [] # 2d points in image plane.
 
     # Make a list of calibration images
-    # images = glob.glob('calibration_wide/GO*.jpg')
-    images = glob.glob('./camera_cal/calibration*.jpg')
+    images = iglob(img_files_regex)
 
     # Step through the list and search for chessboard corners
-    for idx, fname in enumerate(images):
-        img = cv2.imread(fname)
+    for idx, filename in enumerate(images, start=1):
+        img = cv2.imread(filename)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+        ret, corners = get_corners(gray, pts_per_row, pts_per_column)
 
         # If found, add object points, image points
         if ret == True:
@@ -56,50 +40,49 @@ def get_calibration_points(images_regex, num_channels, x, y, z=0):
             imgpoints.append(corners)
 
             # Draw and display the corners
-            cv2.drawChessboardCorners(img, (9,6), corners, ret)
-            #write_name = 'corners_found'+str(idx)+'.jpg'
-            #cv2.imwrite(write_name, img)
-
-            # cv2.imshow('img', img)
-
-            # cv2.waitKey(500)
+            img = draw_image_corners(img, pts_per_row, pts_per_column, corners, ret)
+            write_name = './test_out/corners_found'+str(idx)+'.jpg'
+            cv2.imwrite(write_name, img)
 
     return objpoints, imgpoints
 
 
-def calibrate_camera(objpoints, imgpoints, img_size, output_file):
-    calibration = Calibrator(cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None))
-
-    # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
-    dist_pickle = {}
-    dist_pickle["mtx"] = calibration.mtx
-    dist_pickle["dist"] = calibration.dist
-    pickle.dump(dist_pickle, open(output_file, "wb"))
-
-    return calibration
+def undistort_image(image, matrix, distortion_coefficients):
+    return cv2.undistort(image, matrix, distortion_coefficients, None, matrix)
 
 
-def get_undistorted_img(filename, calibration):
-    img = cv2.imread(filename)
+if __name__ == '__main__':
+    img_files_regex = './camera_cal/calibration*.jpg'
+    pts_per_row = 9
+    pts_per_column = 6
+    num_channels = 3
+
+    objpoints, imgpoints = get_calibration_pts(img_files_regex, pts_per_row, pts_per_column, num_channels)
+
+    # Test undistortion on an image
+    img = cv2.imread('./camera_cal/calibration3.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_size = (img.shape[1], img.shape[0])
-    dst = cv2.undistort(img, calibration.mtx, calibration.dist, None, calibration.mtx)
-    return dst
 
+    # Do camera calibration given object points and image points
+    ret, matrix, distortion_coefficients, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
 
-objpoints, imgpoints = get_calibration_points('./camera_cal/calibration*.jpg', 3, 9, 6, 0)
+    # Save the camera calibration results
+    dist_pickle = {}
+    dist_pickle["mtx"] = matrix
+    dist_pickle["dist"] = distortion_coefficients
+    pickle.dump( dist_pickle, open( "./output_images/calibration.p", "wb" ) )
 
-img_sample = ImageSample('./camera_cal/calibration1.jpg')
+    # Undistort image and save results to file
+    undistorted = undistort_image(img, matrix, distortion_coefficients)
+    undistorted_gray = cv2.cvtColor(undistorted, cv2.COLOR_BGR2GRAY)
+    ret, corners = get_corners(undistorted_gray, pts_per_row, pts_per_column)
+    undistorted = draw_image_corners(undistorted, pts_per_row, pts_per_column, corners, ret)
+    cv2.imwrite('test_out/test_undist.jpg', undistorted)
 
-# Calibrate camera
-calibration = calibrate_camera(objpoints, imgpoints, img_sample.size, './output_images/wide_dist_pickle.p')
-
-# Test undistortion on an image
-dst = get_undistorted_img(img_sample.filename, calibration)
-cv2.imwrite('./output_images/test_undist.jpg', dst)
-
-# Visualize undistortion
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-ax1.imshow(img_sample.image)
-ax1.set_title('Original Image', fontsize=30)
-ax2.imshow(dst)
-ax2.set_title('Undistorted Image', fontsize=30)
+    # Visualize distortion correction
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=30)
+    ax2.imshow(undistorted)
+    ax2.set_title('Undistorted Image', fontsize=30)
